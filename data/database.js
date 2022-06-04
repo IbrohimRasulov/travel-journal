2997ffeddca0e84f0acdebb19aaa43aa17bf68b2
Ibrohim <ibrohim05062000@gmail.com>
2022-06-04 21:36:33 +0500

    Create database sample and components

---
+++ data/database.js
@@ -0,0 +1,57 @@
+export const destination = [
+  {
+    id: 1,
+    title: "Hazrat Imam Complex",
+    country: "Uzbekistan",
+    city: "Tashkent",
+    googleMapsUrl: "#",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat beatae commodi quibusdam eum, fuga eaque nisi? Ratione rem tenetur a, quo, perspiciatis praesentium, dolores veritatis voluptatem nulla commodi harum.",
+    vikipedia: "#",
+    imageUrl: "https://www.journalofnomads.com/wp-content/uploads/2019/11/Hazrat-Imam-Complex-Tashkent-Uzbekistan.jpg",
+    moreImages: "https://www.journalofnomads.com/things-to-do-in-tashkent-travel-guide/#1_See_the_worlds_oldest_Koran_at_the_Hazrat_Imam_Complex"
+  },
+  {
+    id: 2,
+    title: "Hazrat Imam Complex",
+    country: "Uzbekistan",
+    city: "Tashkent",
+    googleMapsUrl: "#",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat beatae commodi quibusdam eum, fuga eaque nisi? Ratione rem tenetur a, quo, perspiciatis praesentium, dolores veritatis voluptatem nulla commodi harum.",
+    vikipedia: "#",
+    imageUrl: "https://www.journalofnomads.com/wp-content/uploads/2019/11/Hazrat-Imam-Complex-Tashkent-Uzbekistan.jpg",
+    moreImages: "https://www.journalofnomads.com/things-to-do-in-tashkent-travel-guide/#1_See_the_worlds_oldest_Koran_at_the_Hazrat_Imam_Complex"
+  },
+  {
+    id: 3,
+    title: "Hazrat Imam Complex",
+    country: "Uzbekistan",
+    city: "Tashkent",
+    googleMapsUrl: "#",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat beatae commodi quibusdam eum, fuga eaque nisi? Ratione rem tenetur a, quo, perspiciatis praesentium, dolores veritatis voluptatem nulla commodi harum.",
+    vikipedia: "#",
+    imageUrl: "https://www.journalofnomads.com/wp-content/uploads/2019/11/Hazrat-Imam-Complex-Tashkent-Uzbekistan.jpg",
+    moreImages: "https://www.journalofnomads.com/things-to-do-in-tashkent-travel-guide/#1_See_the_worlds_oldest_Koran_at_the_Hazrat_Imam_Complex"
+  },
+  {
+    id: 4,
+    title: "Hazrat Imam Complex",
+    country: "Uzbekistan",
+    city: "Tashkent",
+    googleMapsUrl: "#",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat beatae commodi quibusdam eum, fuga eaque nisi? Ratione rem tenetur a, quo, perspiciatis praesentium, dolores veritatis voluptatem nulla commodi harum.",
+    vikipedia: "#",
+    imageUrl: "https://www.journalofnomads.com/wp-content/uploads/2019/11/Hazrat-Imam-Complex-Tashkent-Uzbekistan.jpg",
+    moreImages: "https://www.journalofnomads.com/things-to-do-in-tashkent-travel-guide/#1_See_the_worlds_oldest_Koran_at_the_Hazrat_Imam_Complex"
+  },
+  {
+    id: 5,
+    title: "Hazrat Imam Complex",
+    country: "Uzbekistan",
+    city: "Tashkent",
+    googleMapsUrl: "#",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat beatae commodi quibusdam eum, fuga eaque nisi? Ratione rem tenetur a, quo, perspiciatis praesentium, dolores veritatis voluptatem nulla commodi harum.",
+    vikipedia: "#",
+    imageUrl: "https://www.journalofnomads.com/wp-content/uploads/2019/11/Hazrat-Imam-Complex-Tashkent-Uzbekistan.jpg",
+    moreImages: "https://www.journalofnomads.com/things-to-do-in-tashkent-travel-guide/#1_See_the_worlds_oldest_Koran_at_the_Hazrat_Imam_Complex"
+  },
+]
--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './SASS/App.scss';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Container from './components/Container';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Header />
+      <Container />
+      <Footer />
     </div>
   );
 }
