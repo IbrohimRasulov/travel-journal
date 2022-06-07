f68b13cce46f3d55861caceb7ac89f5e5b7c4fd3
Ibrohim <ibrohim05062000@gmail.com>
2022-06-07 05:00:35 +0500

    Add animation on scroll effects to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,21 @@
-import singlePin from '../assets/singlePin.png'
-import information from '../assets/information.png'
+import singlePin from '../assets/singlePin.png';
+import information from '../assets/information.png';
+
+//* Animate on scroll library
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+//* Settings for the library
+AOS.init({
+  offset: 180, // offset (in px) from the original trigger point
+  duration: 700, // values from 0 to 3000, with step 50ms
+});
 
 function Card(props) {
   let { title, city, googleMapsUrl, description, wikipedia, imageUrl, moreImages } = props.item;
 
   return (
-    <div className="card">
+    <div className="card" data-aos="fade-right">
       <div className="card--img">
         <img src={imageUrl} alt="place" />
         <a href={moreImages} target="_blank" rel="noreferrer">
